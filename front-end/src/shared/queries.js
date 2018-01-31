6ce80b81110d43610b3613223f08047e2e82cf33
Justin Winter <justinlevi@gmail.com>
2018-01-31 13:41:22 -0500

    refactoring graphql queries and mutations out

---
+++ front-end/src/shared/queries.js
@@ -0,0 +1,69 @@
+import gql from 'graphql-tag';
+
+export const currentUserUid = gql `
+query{
+  currentUserContext{
+    uid,
+    uuid
+  }
+}
+`;
+
+export const nodeTitlesByUserQuery = gql `
+query nodeQuery($uid: Int) {
+  nodeQuery(offset:0, limit: 10, filter:{uid:$uid}){
+    entities{
+      ...on NodePage {
+        title,
+        nid,
+        uuid
+        images:fieldMediaImage{
+          mid:targetId
+        }
+      }
+    }
+  }
+}
+`;
+
+export const addPageMutation = gql `
+mutation addPage($title: String!){
+  addPage(input: {title: $title}){
+    entity{
+      ...on NodePage {
+        nid,
+        uuid
+      }
+    }
+  }
+}
+`;
+
+export const getSignedUrls = gql `
+query signedUploadURL ($input: SignedUploadInput!) {
+  signedUploadURL(input:$input)
+}
+`;
+
+export const addS3Files = gql `
+mutation addS3Files($input: S3FilesInput!) {
+  addS3Files(input:$input){
+    mid
+  }
+}
+`;
+
+export const updatePageMutation = gql `
+  mutation updatePage($id:Int!, $field_media_image:[Int]){
+    updatePage(id:$id,input:{
+      field_media_image:$field_media_image
+    }){
+      page:entity{
+        ...on NodePage {
+          nid,
+          uuid
+        }
+      }
+    }
+  }
+`;
