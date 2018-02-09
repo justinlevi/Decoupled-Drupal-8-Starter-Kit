fdf3518533bef1c4b2cea49a030e7e1734d8492f
Khang <khang@Khangs-MacBook-Pro.local>
2018-02-09 09:28:51 -0500

    Removed watch apollo from rootSaga

--- front-end/src/redux/rootSaga.js
+++ front-end/src/redux/rootSaga.js
@@ -1,13 +1,11 @@
 import {all} from 'redux-saga/effects';
 import {watchCsrfToken} from 'redux/auth/csrf/sagas';
-import {watchApolloClient} from 'redux/auth/apollo/sagas';
 import {watchOAuth} from 'redux/auth/oauth/sagas';
 
 // single entry point to start all Sagas at once
 export default function* rootSaga(state) {
   yield all([
     watchCsrfToken(),
-    watchOAuth(),
-    watchApolloClient()
+    watchOAuth()
   ]);
 }
