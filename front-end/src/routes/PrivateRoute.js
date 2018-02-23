a9485a43d743636f757c28eccbdc074303445fc7
Justin Winter <justinlevi@gmail.com>
2018-02-23 10:35:22 -0500

    rewriting as pure function

--- front-end/src/routes/PrivateRoute.js
+++ front-end/src/routes/PrivateRoute.js
@@ -1,66 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Route } from 'react-router';
 import { Redirect } from 'react-router-dom';
 
-// class PrivateRouteContainer extends Component {
-//   render() {
-//     const {
-//       isAuthenticated,
-//       isLoggingIn,
-//       component: Component,
-//       ...props
-//     } = this.props;
+const RedirectToLogin = () => (
+  <Redirect to={{ pathname: '/login' }} />
+);
 
-//     return (
-//       <Route
-//         {...props}
-//         render={props =>
-//           (isAuthenticated || isLoggingIn
-//             ? <Component {...props} />
-//             : (
-//               <Redirect to={{
-//               pathname: '/login',
-//               state: { from: props.location },
-//             }}
-//               />
-//           ))
-//         }
-//       />
-//     );
-//   }
-// }
-
-
-const PrivateRouteContainer = (props) => {
-  const {
-    isAuthenticated,
-    isLoggingIn,
-  } = props;
-
-  return (
-    <Route
-      {...props}
-      render={() =>
-        (isAuthenticated || isLoggingIn
-          ? <Component {...props} />
-          : (
-            <Redirect to={{
-            pathname: '/login',
-            state: { from: props.location },
-          }}
-            />
-        ))
-      }
-    />
-  );
-};
+const PrivateRouteContainer = props => (
+  <Route
+    {...props}
+    component={props.isAuthenticated || props.isLoggingIn ?
+      props.component :
+      RedirectToLogin}
+  />
+);
 
 PrivateRouteContainer.propTypes = {
-  location: PropTypes.string.isRequired,
   isAuthenticated: PropTypes.bool.isRequired,
   isLoggingIn: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
 };
 
 const PrivateRoute = connect(state => ({
