9f1e43b6a604fc1535e6b26d12516c589a5a34b5
Justin Winter <justinlevi@gmail.com>
2018-02-28 10:16:51 -0500

    try try again - more manual approach

--- front-end/node-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./docroot/themes/contrib/cog/STARTERKIT/install-node.sh 8.9.1 &&
-source ~/.bashrc  && nvm use --delete-prefix 8.9.1 &&
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm use 8.9.1 && npm install
