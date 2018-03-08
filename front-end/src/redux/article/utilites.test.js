2a1b6967357e9f1c742949a4bc74954e7bba2865
Khang <khang@Khangs-MacBook-Pro.local>
2018-03-08 10:46:28 -0500

    Added test for updating an article and get the article with nid

--- front-end/src/redux/article/utilites.test.js
+++ front-end/src/redux/article/utilites.test.js
@@ -3,6 +3,43 @@ import * as data from '../../api/__mocks__/data';
 
 const result = data.ARTICLES_BY_USER_DATA;
 const { articles } = result.data.user.nodes;
+const pageModel = {
+    author: {
+      name: 'admin',
+    },
+    title: 'New Title',
+    body: {
+      value: '<p>asdfasdf</p>\r\n',
+    },
+    nid: 14,
+    uuid: 'be510cd5-645e-4f72-8baa-06cf89f53f84',
+    images: [],
+  };
+
+const updatedArticle = [
+  {
+    author: {
+      name: 'admin',
+    },
+    title: 'hello article update',
+    body: null,
+    nid: 13,
+    uuid: '79502776-61f8-4c48-b464-d94eebe0e01b',
+    images: [],
+  },
+  {
+    author: {
+      name: 'admin',
+    },
+    title: 'New Title',
+    body: {
+      value: '<p>asdfasdf</p>\r\n',
+    },
+    nid: 14,
+    uuid: 'be510cd5-645e-4f72-8baa-06cf89f53f84',
+    images: [],
+  },
+];
 
 describe('articles utilities tests', () => {
   it('format fetch article result', () => {
@@ -17,13 +54,25 @@ describe('articles utilities tests', () => {
     expect(actual).toEqual(expected);
   });
 
-  // TO DO: WRITE THIS TEST
   it('updates an article', () => {
-    expect(1).toEqual(1);
+    //The expected article array with the updated article
+    const expected = updatedArticle;
+
+    //Passing in the original article mock data, and the updated page.
+    const actual = utilities.updateArticlesWithArticle(articles,pageModel);
+
+    //Expecting the output to be the updatedArticle
+    expect(expected).toEqual(actual);
   });
 
-  // TO DO: WRITE THIS TEST
   it('gets the article from the nid', () => {
-    expect(1).toEqual(1);
+    //The expect article object
+    const expected = articles[1];
+
+    //Passing in the article we want to extract
+    const actual = utilities.getArticleFromNid(articles,14);
+
+    //Expecting the output to be the article object matching the nid of 14
+    expect(actual).toEqual(expected);
   });
 });
