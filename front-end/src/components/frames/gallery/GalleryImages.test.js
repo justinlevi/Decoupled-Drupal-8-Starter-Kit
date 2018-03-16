3933115d91cd1a762267f8f67a8b7b4f9286a3b4
Justin Winter <justinlevi@gmail.com>
2018-03-16 11:33:05 -0400

    fixing error after rename

--- front-end/src/components/frames/gallery/GalleryImages.test.js
+++ front-end/src/components/frames/gallery/GalleryImages.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
-import GalleryImages from './GalleryImages';
+import { GalleryImages } from './GalleryImages';
 
 const initalPropState = {
   images: [],
