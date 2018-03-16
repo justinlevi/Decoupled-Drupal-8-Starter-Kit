aadea19bde9c021ac6b4479fee8e2f1c36cb9066
Justin Winter <justinlevi@gmail.com>
2018-03-16 14:17:28 -0400

    regression fix

--- front-end/src/redux/localStorage.test.js
+++ front-end/src/redux/localStorage.test.js
@@ -5,6 +5,5 @@ describe('', () => {
   it('should do the thing', () => {
     expect(localStorage.loadState()).toEqual({ Hey: 'Ho' });
     expect(localStorage.loadState()).toEqual(undefined);
-    expect(localStorage.loadState()).toEqual(undefined);
   });
 });
