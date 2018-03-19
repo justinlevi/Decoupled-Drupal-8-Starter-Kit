99631134d5b930d39e95ecb148e27fb541a20982
Khang <khang@Khangs-MacBook-Pro.local>
2018-03-19 12:45:00 -0400

    Added save article updates saga to test

--- front-end/src/redux/article/sagas.test.js
+++ front-end/src/redux/article/sagas.test.js
@@ -6,7 +6,7 @@ import * as oauthActions from '../auth/oauth/actions';
 import {introspectionQuery, buildClientSchema, graphql} from 'graphql';
 import {addMockFunctionsToSchema} from 'graphql-tools';
 import {print} from 'graphql/language/printer';
-import { put,call,takeEvery,select } from 'redux-saga/effects';
+import { put,call,takeEvery,take,select } from 'redux-saga/effects';
 import * as introspectionResult from '../../api/schema.json';
 import {
   CURRENT_USER_QUERY,
@@ -25,7 +25,7 @@ import * as data from '../../api/__mocks__/data';
 
 const articles = data.ARTICLES_BY_USER_DATA;
 
-import { articlesByUser,createArticle,selectArticles,deleteArticle } from '../../api/apolloProxy';
+import { articlesByUser,createArticle,selectArticles,deleteArticle,updateArticle } from '../../api/apolloProxy';
 import { formatFetchArticlesResult, removeArticleFromArticles, updateArticlesWithArticle, getArticleFromNid } from './utilities';
 import {
   types as articleActionTypes,
@@ -212,7 +212,49 @@ describe('the sagas', () => {
 
     })
 
+  }); //End of Delete Article Saga test
 
-  });
+  it('Should execute saveArticleUpdatesSaga and succeed',() => {
+
+    const action ={
+      payload: {
+        id: 1,
+        title: 'asdfasd',
+        body: 'body text',
+        field_media_image: [1, 2, 3],
+      }
+    }
+
+    const saga = expectSaga(sagas.saveArticleUpdatesSaga,action);
+
+    graphql(schema, print(updateArticleMutation), null, null, action.payload).then((result) => {
+
+      result.data.updateArticle.page = {
+        author: {
+          name: 'admin'
+        },
+        title: 'Whatsup',
+        body: null,
+        nid: 13,
+        uuid: '79502776-61f8-4c48-b464-d94eebe0e01b',
+        images: []
+      };
+
+      const page = result.data.updateArticle.page;
+      const testData = updateArticlesWithArticle(storeState.articleReducer.articles, page);
+
+      saga
+      .provide([
+        [matchers.call.fn(updateArticle), result],
+      ])
+      .withState(storeState)
+      .put({type: 'TOKENS_EXPIRED_CHECK'})
+      .put(saveArticleUpdatesSuccess({ articles: testData }))
+      .dispatch({type: oauthActions.TOKENS_EXPIRED_CHECK_VALID})
+      .run()
+
+    })
+
+  });//End of Save Article Updates Saga
 
 });
