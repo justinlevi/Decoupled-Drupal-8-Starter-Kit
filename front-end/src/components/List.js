57d181d21bbcd740792901df7a4d871011d0f08d
Khang <khang@Khangs-MacBook-Pro.local>
2018-04-09 13:35:37 -0400

    Working search

--- front-end/src/components/List.js
+++ front-end/src/components/List.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes, { arrayOf, shape } from 'prop-types';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
@@ -29,51 +29,103 @@ const ListItems = ({ articles, selectHandler, deleteHandler }) => articles.map(p
   </Fade>
 ));
 
-const List = (props) => {
-  const {
-    addHandler, isModalVisible, onDeleteModalToggle, onDeleteModalOk,
-  } = props;
-  return (
-    <div className="">
-      <div className="container">
-        <div
-          role="button"
-          tabIndex={0}
-          onKeyUp={() => { addHandler(); }}
-          className="py-3"
-          onClick={() => { addHandler(); }}
-        >
-          <div className="add">
-            <MdAdd />
-            <h2 className="card-title">Add</h2>
+class List extends Component{
+
+  state = {
+    articles: this.props.articles
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState){
+    if(nextProps.articles.length != prevState.articles.length){
+      return {
+        articles: nextProps.articles
+      };
+    }
+    return null;
+  }
+
+  handleSearchInput = (target) => {
+
+    const searchTerm = target.value;
+    const articles = this.props.articles;
+    const searchedArticles = articles.map(page => {
+      const title = page.title.toLowerCase();
+      const searchTermLower = searchTerm.toLowerCase();
+      return title.includes(searchTermLower) ? page: null;
+    });
+    const filteredArticles = searchedArticles.filter((n) => n != null);
+
+    if(filteredArticles.length > 0){
+      this.setState({
+        articles: filteredArticles
+      })
+    }else if(target.value === ''){
+      this.setState({
+        articles: this.props.articles
+      })
+    }else{
+      this.setState({
+        articles: []
+      })
+    }
+
+  }
+
+  render(){
+    const {
+      addHandler, isModalVisible, onDeleteModalToggle, onDeleteModalOk,selectHandler, deleteHandler
+    } = this.props;
+
+    return (
+      <div className="">
+        <div className="container">
+          <div
+            role="button"
+            tabIndex={0}
+            onKeyUp={() => { addHandler(); }}
+            className="py-3"
+            onClick={() => { addHandler(); }}
+          >
+            <div className="add">
+              <MdAdd />
+              <h2 className="card-title">Add</h2>
+            </div>
           </div>
-        </div>
 
-        <div className="input-group input-group-lg">
-          <div className="input-group-prepend">
-            <span className="input-group-text" id="inputGroup-sizing-lg">Search By Title</span>
+          <div className="input-group input-group-lg">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-lg">Search By Title</span>
+            </div>
+            <input onChange={({target}) => this.handleSearchInput(target)} placeholder="Content Title..." type="text" className="form-control search-bar" aria-label="Large" aria-describedby="inputGroup-sizing-sm"/>
           </div>
-          <input placeholder="Content Title..." type="text" className="form-control search-bar" aria-label="Large" aria-describedby="inputGroup-sizing-sm"/>
+
+          {
+            this.state.articles.length > 0 ?
+            <TransitionGroup className="item-list">
+              <ListItems
+                articles={this.state.articles}
+                selectHandler={selectHandler}
+                deleteHandler={deleteHandler}
+               />
+            </TransitionGroup>
+          : <div className="no-result-text h1 text-center">No Results Found</div>
+          }
+
+          <Modal isOpen={isModalVisible} toggle={onDeleteModalToggle} backdrop>
+            <ModalHeader toggle={onDeleteModalToggle}>Confirmation</ModalHeader>
+            <ModalBody>
+              Are you sure you want to remove this?
+            </ModalBody>
+            <ModalFooter>
+              <Button color="secondary" onClick={onDeleteModalToggle}>Cancel</Button>
+              {' '}
+              <Button color="primary" onClick={onDeleteModalOk}>Delete</Button>
+            </ModalFooter>
+          </Modal>
         </div>
-
-        <TransitionGroup className="item-list">
-          <ListItems {...props} />
-        </TransitionGroup>
-
-        <Modal isOpen={isModalVisible} toggle={onDeleteModalToggle} backdrop>
-          <ModalHeader toggle={onDeleteModalToggle}>Confirmation</ModalHeader>
-          <ModalBody>
-            Are you sure you want to remove this?
-          </ModalBody>
-          <ModalFooter>
-            <Button color="secondary" onClick={onDeleteModalToggle}>Cancel</Button>
-            {' '}
-            <Button color="primary" onClick={onDeleteModalOk}>Delete</Button>
-          </ModalFooter>
-        </Modal>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 List.propTypes = {
