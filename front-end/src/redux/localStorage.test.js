8dbd3a3ea6a6345639e83415290db44edc97feed
Justin Winter <justinlevi@gmail.com>
2018-04-09 14:53:28 -0400

    removing redux

--- front-end/src/redux/localStorage.test.js
+++
@@ -1,9 +0,0 @@
-
-import * as localStorage from './localStorage';
-
-describe('', () => {
-  it('should do the thing', () => {
-    expect(localStorage.loadState()).toEqual({ Hey: 'Ho' });
-    expect(localStorage.loadState()).toEqual(undefined);
-  });
-});
