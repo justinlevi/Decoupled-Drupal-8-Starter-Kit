b4a4ef67b7fc3483a503b1f6c6612ae14726ddab
Khang <khang@Khangs-MacBook-Pro.local>
2018-04-10 11:23:40 -0400

    removed some not needed imports

--- front-end/src/api/apolloClient.js
+++ front-end/src/api/apolloClient.js
@@ -6,7 +6,7 @@ import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { withClientState } from 'apollo-link-state';
-import { ApolloLink, Observable } from 'apollo-link';
+import { ApolloLink } from 'apollo-link';
 
 import { defaults, resolvers } from './resolvers';
 import introspectionQueryResultData from './fragmentTypes.json';
@@ -16,43 +16,21 @@ const URL = process.env.REACT_APP_HOST_DOMAIN ? process.env.REACT_APP_HOST_DOMAI
 
 //const csrf = sessionStorage.getItem('csrfToken') || null;
 const fragmentMatcher = new IntrospectionFragmentMatcher({ introspectionQueryResultData });
-
-const requestLink = new ApolloLink((operation, forward) =>
-  new Observable(observer => {
-    let handle: any;
-    Promise.resolve(operation)
-      .then(oper => request(oper))
-      .then(() => {
-        handle = forward(operation).subscribe({
-          next: observer.next.bind(observer),
-          error: observer.error.bind(observer),
-          complete: observer.complete.bind(observer),
-        });
-      })
-      .catch(observer.error.bind(observer));
-
-    return () => {
-      if (handle) handle.unsubscribe();
-    };
-  })
-);
-
-const request = async (operation) => {
-  const authToken = await localStorage.getItem('authToken') || null;
-  //const xcsrf = csrf || await axios.get(`${URL}/session/token`);
-
-  if (authToken !== null) {
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${authToken}`
-      }
-    });
-  }
-};
-
 const cache = new InMemoryCache({ fragmentMatcher });
 
 const client = new ApolloClient({
+  request: async (operation) => {
+    const authToken = await localStorage.getItem('authToken') || null;
+    //const xcsrf = csrf || await axios.get(`${URL}/session/token`);
+
+    if (authToken !== null) {
+      operation.setContext({
+        headers: {
+          authorization: `Bearer ${authToken}`
+        }
+      });
+    }
+  },
   link: ApolloLink.from([
     onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors) {
@@ -61,7 +39,6 @@ const client = new ApolloClient({
       }
       if (networkError) console.log(`[Network error]: ${networkError}`);
     }),
-    requestLink,
     withClientState({
       defaults,
       resolvers,
