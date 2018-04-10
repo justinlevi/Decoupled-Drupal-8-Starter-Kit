82ac6d98ca5549913e88930758ff2eba03730bcb
Khang <khang@Khangs-MacBook-Pro.local>
2018-04-10 13:04:31 -0400

    Removed httplink

--- front-end/src/api/apolloClient.js
+++ front-end/src/api/apolloClient.js
@@ -3,7 +3,6 @@
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 
 import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { withClientState } from 'apollo-link-state';
 import { ApolloLink } from 'apollo-link';
