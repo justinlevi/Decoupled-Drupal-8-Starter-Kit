72388fde035404e5c385fe453f1953247885f266
Khang <khang@Khangs-MacBook-Pro.local>
2018-04-11 09:52:45 -0400

    Tiles file clean up

--- front-end/src/components/Tile.js
+++ front-end/src/components/Tile.js
@@ -2,23 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Tile = (props) => {
-  const articles = props.articles;
-  return(
+  const { articles } = props;
+  return (
     <div className="tiles-wrapper row">
-      { articles ? articles.map((val,index)=> {
-        return (
-          <div className="card col-sm" key={index}>
-            {val.image ? <img className="card-img-top" src={val.image} alt="Card cap" /> : <img src="https://placeholdit.imgix.net/~text?txtsize=38&amp;txt=¯\_(ツ)_/¯&amp;w=400&amp;h=400" alt="" className="w-100" />}
-            <div className="card-body">
-              <h5 className="card-title">{val.label}</h5>
-            </div>
+      { articles ? articles.map(val => (
+        <div className="card col-sm">
+          {val.image ? <img className="card-img-top" src={val.image} alt="Card cap" /> : <img src="https://placeholdit.imgix.net/~text?txtsize=38&amp;txt=¯\_(ツ)_/¯&amp;w=400&amp;h=400" alt="" className="w-100" />}
+          <div className="card-body">
+            <h5 className="card-title">{val.label}</h5>
           </div>
-        )
-      }) : null }
+        </div>
+        )) : null }
 
     </div>
-  )
-}
+  );
+};
 
 Tile.propTypes = {
   articles: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
