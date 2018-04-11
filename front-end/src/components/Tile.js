6fc6b15d438ab69ed41a9252044ecf49788d422b
Khang <khang@Khangs-MacBook-Pro.local>
2018-04-11 09:54:45 -0400

    More linting

--- front-end/src/components/Tile.js
+++ front-end/src/components/Tile.js
@@ -7,7 +7,19 @@ const Tile = (props) => {
     <div className="tiles-wrapper row">
       { articles ? articles.map(val => (
         <div className="card col-sm">
-          {val.image ? <img className="card-img-top" src={val.image} alt="Card cap" /> : <img src="https://placeholdit.imgix.net/~text?txtsize=38&amp;txt=¯\_(ツ)_/¯&amp;w=400&amp;h=400" alt="" className="w-100" />}
+          {
+            val.image ?
+              <img
+                className="card-img-top"
+                src={val.image}
+                alt="Card cap"
+              /> :
+              <img
+                src="https://placeholdit.imgix.net/~text?txtsize=38&amp;txt=¯\_(ツ)_/¯&amp;w=400&amp;h=400"
+                alt=""
+                className="w-100"
+              />
+            }
           <div className="card-body">
             <h5 className="card-title">{val.label}</h5>
           </div>
