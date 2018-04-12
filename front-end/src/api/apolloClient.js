e2f60653de6b10742b2a557c4752bcbeedc2bc39
Khang <khang@Khangs-MacBook-Pro.local>
2018-04-12 10:13:20 -0400

    Observer added

--- front-end/src/api/apolloClient.js
+++ front-end/src/api/apolloClient.js
@@ -1,12 +1,10 @@
-//import axios from 'axios';
-
+import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 
-import { ApolloClient } from 'apollo-client';
 import { onError } from 'apollo-link-error';
 import { withClientState } from 'apollo-link-state';
-import { ApolloLink } from 'apollo-link';
-import { createUploadLink } from 'apollo-upload-client'
+import { ApolloLink, Observable } from 'apollo-link';
+import { createUploadLink } from 'apollo-upload-client';
 
 import { defaults, resolvers } from './resolvers';
 import introspectionQueryResultData from './fragmentTypes.json';
@@ -14,42 +12,61 @@ import introspectionQueryResultData from './fragmentTypes.json';
 const POSTFIX = process.env.REACT_APP_XDEBUG_POSTFIX;
 const URL = process.env.REACT_APP_HOST_DOMAIN ? process.env.REACT_APP_HOST_DOMAIN : '';
 
-//const csrf = sessionStorage.getItem('csrfToken') || null;
+// const csrf = sessionStorage.getItem('csrfToken') || null;
 const fragmentMatcher = new IntrospectionFragmentMatcher({ introspectionQueryResultData });
 const cache = new InMemoryCache({ fragmentMatcher });
 
-const client = new ApolloClient({
-  request: async (operation) => {
-    const authToken = await localStorage.getItem('authToken') || null;
-    //const xcsrf = csrf || await axios.get(`${URL}/session/token`);
+const request = (operation) => {
+  const token = localStorage.getItem('token') || null;
+  operation.setContext({
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+};
 
-    if (authToken !== null) {
-      operation.setContext({
-        headers: {
-          authorization: `Bearer ${authToken}`
-        }
-      });
-    }
-  },
+const requestLink = new ApolloLink((operation, forward) =>
+  new Observable((observer) => {
+    let handle: any;
+    Promise.resolve(operation)
+      .then(oper => request(oper))
+      .then(() => {
+        handle = forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer),
+        });
+      })
+      .catch(observer.error.bind(observer));
+
+    return () => {
+      if (handle) handle.unsubscribe();
+    };
+  }));
+
+const client = new ApolloClient({
   link: ApolloLink.from([
     onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors) {
         graphQLErrors.map(({ message, locations, path }) =>
           console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`));
       }
-      if (networkError) console.log(`[Network error]: ${networkError}`);
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+      }
     }),
+    requestLink,
     withClientState({
       defaults,
       resolvers,
-      cache
+      cache,
     }),
     createUploadLink({
-      uri: URL + '/graphql' + POSTFIX,
-      credentials: 'include'
+      uri: `${URL}/graphql${POSTFIX}`,
+      credentials: 'include',
     }),
   ]),
-  cache
+  cache,
 });
 
 export default client;
